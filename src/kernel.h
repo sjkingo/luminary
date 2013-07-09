2465aebba8ce864604691be4ddab6212529c768e
Sam Kingston <sam@sjkwi.com.au>
2013-07-10 01:19:08 +1000

    remove start and end addresses of kernel linkage

--- src/kernel.h
+++ src/kernel.h
@@ -1,10 +1,6 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-/* Start and end addresses of the kernel, as provided by the linker */
-extern int _kernel_start;
-extern int _kernel_end;
-
 struct multiboot_info *mb_info;
 
 /* printf() for the kernel */
