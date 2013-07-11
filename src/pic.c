e6f4e751bf4abaa9d37bed111fe66f1fbe7efaf5
Sam Kingston <sam@sjkwi.com.au>
2013-07-12 06:56:06 +1000

    set up the PIT system timer

--- src/pic.c
+++ src/pic.c
@@ -12,6 +12,14 @@ static inline void ack_irq(int irq)
     outb(PIC_MASTER_CMD, PIC_EOI);
 }
 
+static void timer_init(void)
+{
+    unsigned int div = PIT_CLOCK / TIMER_FREQ;
+    outb(PIT_CMD_PORT, 0x36); /* command byte */
+    outb(PIT_CH0_DATA, (unsigned char)(div & 0xFF));
+    outb(PIT_CH0_DATA, (unsigned char)((div >> 8) & 0xFF));
+}
+
 void pic_init(void)
 {
     /* Re-map the IRQ table to CPU vectors [32..48] */
@@ -24,14 +32,24 @@ void pic_init(void)
     outb(PIC_MASTER_DATA, PIC_8086);
     outb(PIC_SLAVE_DATA, PIC_8086);
 
+    /* Set up the PIT chip here, before masking PIC_MASK_TIMER, otherwise we
+     * will get spurious timer interrupts at a BIOS-set frequency.  Note this
+     * really shouldn't matter because interrupts will already be disabled!
+     */
+    timer_init();
+
     /* mask the interrupts we want to receive */
-    outb(PIC_MASTER_DATA, PIC_MASK_ALL);
+    outb(PIC_MASTER_DATA, PIC_MASK_ALL & PIC_MASK_TIMER);
     outb(PIC_SLAVE_DATA, PIC_MASK_ALL);
 }
 
 void irq_handler(struct trap_frame frame)
 {
     switch (frame.trapno) {
+        case IRQ_TIMER:
+            // TODO
+            goto out;
+
         default:
             printk("Unhandled IRQ%d\n", frame.trapno);
             goto out;
--- src/pic.h
+++ src/pic.h
@@ -1,12 +1,18 @@
 #ifndef PIC_H
 #define PIC_H
 
-/* pic.h - the Intel 8259 Programmable Interrupt Controller */
+/* pic.h - the Intel 8259 Programmable Interrupt Controller.
+ * Also contains definitions for the PIT (8253/8254) timer chip.
+ */
 
 #include "traps.h"
 
 #define IRQ_BASE_OFFSET 32
 #define IRQ_SLAVE_OFFSET (IRQ_BASE_OFFSET + 8)
+
+enum pic_interrupts {
+    IRQ_TIMER           = IRQ_BASE_OFFSET + 0,  // system timer
+};
 
 /* PIC ports */
 #define PIC_MASTER_CMD  0x20
@@ -25,6 +31,17 @@
  *   to disable line: bitwise | with current mask
  */
 #define PIC_MASK_ALL            0xFF
+#define PIC_MASK_TIMER          ~(1 << (0 & 0x7))       // IRQ 0
+
+/* PIT (system timer) definitions.
+ * See http://wiki.osdev.org/Programmable_Interval_Timer
+ */
+#define PIT_CH0_DATA    0x40
+#define PIT_CH1_DATA    (PIT_CH0_DATA + 1)
+#define PIT_CH2_DATA    (PIT_CH0_DATA + 2)
+#define PIT_CMD_PORT    0x43
+#define PIT_CLOCK       1193180
+#define TIMER_FREQ      50      // ms
 
 /* init the PIC */
 void pic_init(void);
