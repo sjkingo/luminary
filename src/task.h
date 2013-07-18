942a12a35a660233fcb70be664d317a7e493f2f1
Sam Kingston <sam@sjkwi.com.au>
2013-07-18 21:17:21 +1000

    swap the priority levels of suspended tasks and the idle task

--- src/task.h
+++ src/task.h
@@ -16,8 +16,8 @@ extern struct task *sched_queue;
 extern struct task *running_task;
 
 /* Scheduling levels. See the comment in sched.c for more info. */
-#define SCHED_LEVEL_SUSP        (-1)
-#define SCHED_LEVEL_IDLE        0
+#define SCHED_LEVEL_SUSP        0
+#define SCHED_LEVEL_IDLE        (-1)
 #define SCHED_LEVEL_MIN         1
 #define SCHED_LEVEL_MAX         10
 
